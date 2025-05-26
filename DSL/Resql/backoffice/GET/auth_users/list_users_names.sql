d9364b7176637d024057928c1412a6937aaae24e
ahmer-mt <ahmer.ali@mindtitan.com>
2025-05-26 13:07:11 +0500

    ended timestamp duplication fix

--- DSL/Resql/backoffice/GET/auth_users/list_users_names.sql
+++ DSL/Resql/backoffice/GET/auth_users/list_users_names.sql
@@ -1,5 +1,7 @@
 SELECT DISTINCT ON (id_code)
                     id_code,
-                    display_name
+                    display_name,
+                    first_name,
+                    last_name
                 FROM "user"
                 ORDER BY id_code ASC, created DESC;
